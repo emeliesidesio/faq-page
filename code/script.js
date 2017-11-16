6f0396187b17e0d49e1eb94cbc90cf1daf32612c
Emelie Sidesiö <esidesio@gmail.com>
2017-11-16 16:18:18 +0100

    animation added

--- code/script.js
+++ code/script.js
@@ -1,6 +1,17 @@
 // A function that adds and remove the class "active" on the section you click on.
-function toggle() {
-  this.classList.toggle("active")
+var acc = document.getElementsByClassName("title");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
 }
 
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
